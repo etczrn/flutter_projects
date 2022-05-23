04b3c3df6f26313392e8bc58947b370027e921a1
alex <alex@itdda.io>
2022-05-23 15:26:34 +0900

    Closes #7; Create Stateful widget and initiate state #7

--- global_fitness/lib/screens/bmi_screen.dart
+++ global_fitness/lib/screens/bmi_screen.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:global_fitness/shared/menu_bottom.dart';
 import 'package:global_fitness/shared/menu_drawer.dart';
 
-class BmiScreen extends StatelessWidget {
+class BmiScreen extends StatefulWidget {
   const BmiScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BmiScreen> createState() => _BmiScreenState();
+}
+
+class _BmiScreenState extends State<BmiScreen> {
+  final double fontSize = 18;
+  String result = '';
+  bool isMetric = true;
+  bool isImperial = false;
+  double? height; // ? means nullable
+  double? weight;
+  late List<bool> isSelected;
+
+  @override
+  void initState() {
+    isSelected = [isMetric, isImperial];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +30,36 @@ class BmiScreen extends StatelessWidget {
       appBar: AppBar(title: Text('BMI Calculator')),
       bottomNavigationBar: MenuBottom(),
       drawer: MenuDrawer(),
-      body: Center(child: FlutterLogo()),
+      body: Column(
+        children: [
+          ToggleButtons(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text('Metric', style: TextStyle(fontSize: fontSize))),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child:
+                      Text('Imperial', style: TextStyle(fontSize: fontSize))),
+            ],
+            isSelected: isSelected,
+            onPressed: toggleMeasure,
+          )
+        ],
+      ),
     );
+  }
+
+  void toggleMeasure(value) {
+    if (value == 0) {
+      isMetric = true;
+      isImperial = false;
+    } else {
+      isMetric = false;
+      isImperial = true;
+    }
+    setState(() {
+      isSelected = [isMetric, isImperial];
+    });
   }
 }
