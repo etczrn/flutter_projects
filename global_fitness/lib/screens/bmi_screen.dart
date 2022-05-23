329adcb0a952dac4677708e9c2c5d9b26e6e49fd
alex <alex@itdda.io>
2022-05-23 17:30:58 +0900

    Closes #9; Create calculating BMI function and change view for landscape mode #9

--- global_fitness/lib/screens/bmi_screen.dart
+++ global_fitness/lib/screens/bmi_screen.dart
@@ -38,37 +38,46 @@ class _BmiScreenState extends State<BmiScreen> {
       appBar: AppBar(title: Text('BMI Calculator')),
       bottomNavigationBar: MenuBottom(),
       drawer: MenuDrawer(),
-      body: Column(
-        children: [
-          ToggleButtons(
-            children: [
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Text('Metric', style: TextStyle(fontSize: fontSize))),
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child:
-                      Text('Imperial', style: TextStyle(fontSize: fontSize))),
-            ],
-            isSelected: isSelected,
-            onPressed: toggleMeasure,
-          ),
-          TextField(
-            controller: txtHeight,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(hintText: heightMessage),
-          ),
-          TextField(
-            controller: txtWeight,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(hintText: weightMessage),
-          ),
-          ElevatedButton(
-              onPressed: () {},
-              child:
-                  Text('Calculate BMI', style: TextStyle(fontSize: fontSize))),
-          Text(result, style: TextStyle(fontSize: fontSize)),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ToggleButtons(
+              children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child:
+                        Text('Metric', style: TextStyle(fontSize: fontSize))),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child:
+                        Text('Imperial', style: TextStyle(fontSize: fontSize))),
+              ],
+              isSelected: isSelected,
+              onPressed: toggleMeasure,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(
+                controller: txtHeight,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: heightMessage),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: TextField(
+                controller: txtWeight,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: weightMessage),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: findBMI,
+                child: Text('Calculate BMI',
+                    style: TextStyle(fontSize: fontSize))),
+            Text(result, style: TextStyle(fontSize: fontSize)),
+          ],
+        ),
       ),
     );
   }
@@ -83,6 +92,21 @@ class _BmiScreenState extends State<BmiScreen> {
     }
     setState(() {
       isSelected = [isMetric, isImperial];
+    });
+  }
+
+  void findBMI() {
+    double bmi = 0;
+    double height = double.tryParse(txtHeight.text) ?? 0;
+    double weight = double.tryParse(txtWeight.text) ?? 0;
+
+    if (isMetric) {
+      bmi = weight / (height * height);
+    } else {
+      bmi = (weight * 703) / (height * height);
+    }
+    setState(() {
+      result = 'Your BMI is: ' + bmi.toStringAsFixed(2);
     });
   }
 }
