dfab467e4a65659bfd7f6e7ace6cd9a32d87fe56
alex <alex@itdda.io>
2022-05-23 18:34:06 +0900

    Completing the Weather Screen #11

--- global_fitness/lib/screens/weather_screen.dart
+++ global_fitness/lib/screens/weather_screen.dart
@@ -10,22 +10,65 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  final TextEditingController txtPlace = TextEditingController();
   Weather result = Weather('', '', 0, 0, 0, 0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Weather')),
-        body: Column(
-          children: [
-            ElevatedButton(onPressed: getData, child: Text('Get data')),
-            Text(result.name),
-          ],
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: TextField(
+                  controller: txtPlace,
+                  decoration: InputDecoration(
+                      hintText: 'Enter a city',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: getData,
+                      )),
+                ),
+              ),
+              weatherRow('Place: ', result.name),
+              weatherRow('Description: ', result.description),
+              weatherRow(
+                  'Temperature: ', result.temperature.toStringAsFixed(2)),
+              weatherRow('Perceived: ', result.perceived.toStringAsFixed(2)),
+              weatherRow('Pressure: ', result.pressure.toString()),
+              weatherRow('Humidity: ', result.humidity.toString()),
+            ],
+          ),
         ));
   }
 
   Future getData() async {
     HttpHelper helper = HttpHelper();
-    result = await helper.getWeather('London');
+    result = await helper.getWeather(txtPlace.text);
     setState(() {});
+  }
+
+  Widget weatherRow(String label, String value) {
+    Widget row = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        children: [
+          Expanded(
+              flex: 3,
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 20, color: Theme.of(context).hintColor))),
+          Expanded(
+              flex: 4,
+              child: Text(value,
+                  style: TextStyle(
+                      fontSize: 20, color: Theme.of(context).primaryColor))),
+        ],
+      ),
+    );
+
+    return row;
   }
 }
