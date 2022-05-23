5abb887343337a7f6e67fbbf3f9840640fd49d23
alex <alex@itdda.io>
2022-05-23 10:45:30 +0900

    Closes #2; Change main dart using image and typography with some CSS #2

--- global_fitness/lib/main.dart
+++ global_fitness/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 // entry point
@@ -12,7 +14,29 @@ class GlobalApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // implicit new keyword
     return MaterialApp(
-      home: Center(child: Text('Hello World')),
+      home: Scaffold(
+          appBar: AppBar(title: Text('Global Fitness')),
+          body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/beach.jpg'), fit: BoxFit.cover)),
+            child: Center(
+                child: Container(
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.white70),
+              child: Text(
+                  'Commit to be fit, dare to be great \with Global Fitness',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 22, shadows: [
+                    Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 2.0,
+                        color: Colors.grey)
+                  ])),
+            )),
+          )),
     );
   }
 }
