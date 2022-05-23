e2f347e7b4b5ca80e12d89846f50229e6a1d6392
etczrn <etczrn@gmail.com>
2022-05-23 22:36:01 +0900

    Closes #14; Using an Alert Dialog #14

--- global_fitness/lib/screens/session_screen.dart
+++ global_fitness/lib/screens/session_screen.dart
@@ -10,6 +10,16 @@ class SessionsScreen extends StatefulWidget {
 }
 
 class _SessionsScreenState extends State<SessionsScreen> {
+  final TextEditingController txtDescription = TextEditingController();
+  final TextEditingController txtDuration = TextEditingController();
+  final SPHelper helper = SPHelper();
+
+  @override
+  void initState() {
+    helper.init();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +29,56 @@ class _SessionsScreenState extends State<SessionsScreen> {
       body: Container(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showSessionDialog(context);
+        },
       ),
     );
+  }
+
+  Future saveSession() async {
+    DateTime now = DateTime.now();
+    String today = '${now.year}-${now.month}-${now.day}';
+    Session newSession = Session(
+        1, today, txtDescription.text, int.tryParse(txtDuration.text) ?? 0);
+    helper.writeSession(newSession);
+    txtDescription.text = '';
+    txtDuration.text = '';
+  }
+
+  Future<dynamic> showSessionDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Insert Training Session'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                      controller: txtDescription,
+                      decoration: InputDecoration(
+                        hintText: 'Description',
+                      )),
+                  TextField(
+                      controller: txtDuration,
+                      decoration: InputDecoration(
+                        hintText: 'Duration',
+                      )),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    txtDescription.text = '';
+                    txtDuration.text = '';
+                  },
+                  child: Text('Cancel')),
+              ElevatedButton(onPressed: saveSession, child: Text('Save'))
+            ],
+          );
+        });
   }
 }
