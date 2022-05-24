801a79ce24bbcb8a52806dca7b3926c7a2abf513
etczrn <etczrn@gmail.com>
2022-05-24 20:33:45 +0900

    Closes #20; Setting a Container Size and Shape #20

--- hello_layout/lib/main.dart
+++ hello_layout/lib/main.dart
@@ -5,51 +5,24 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-// Build context describes where you are in the tree of widgets
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          // colorScheme: ColorScheme.fromSwatch(
-          //   // dark mode에서는 AppBar 색상이 바뀌지 않음
-          //   // brightness: Brightness.dark,
-          //   // primarySwatch: Colors.purple,
-          // ).copyWith(secondary: Colors.green),
-          // textTheme: const TextTheme(bodyText2: TextStyle(fontSize: 20))
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-            primary: const Color(0xff009688),
-            secondary: const Color(0xff7c4dff),
-          ),
-          textTheme: const TextTheme(bodyText2: TextStyle(fontSize: 20))),
+      title: 'Your app name',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Building Layouts with Flutter'),
+          title: const Text('Your Title here'),
         ),
-        body: const Center(
-          child: Text(
-            'Hello Flutter Layout',
-          ),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.orange,
+              // child: const Text('Hello container'),
+              width: 100.0,
+              height: 100.0,
+            )
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.lightbulb_outline),
-          onPressed: () {
-            print('You rang?');
-          },
-        ),
-        persistentFooterButtons: [
-          IconButton(
-            icon: const Icon(Icons.add_comment),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.add_alarm),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.add_location),
-            onPressed: () {},
-          ),
-        ],
       ),
     );
   }
