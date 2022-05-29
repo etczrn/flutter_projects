91b4081f3e6da2b9f5f605d9c1fc86eb6eeb5be3
etczrn <etczrn@gmail.com>
2022-05-29 15:20:14 +0900

    Make our password text field secure #34

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -55,11 +55,17 @@ class _HomePageState extends State<HomePage> {
         children: [
           TextField(
             controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
             decoration:
                 const InputDecoration(hintText: 'Enter your email here'),
           ),
           TextField(
             controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
             decoration:
                 const InputDecoration(hintText: 'Enter your password here'),
           ),
