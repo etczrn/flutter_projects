a75b21776aaccf87be8536a3ced1a8a0f5a65b5b
etczrn <etczrn@gmail.com>
2022-06-01 17:16:11 +0900

    Wrap Sacffold with BlocProvider #35

--- bloc_with_joke/lib/presentaion/home/home.dart
+++ bloc_with_joke/lib/presentaion/home/home.dart
@@ -1,42 +1,50 @@
+import 'package:bloc_with_joke/bloc/joke_bloc/joke_bloc.dart';
+import 'package:bloc_with_joke/data/repositories/joke_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('The Joke App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const ExpansionTile(
-              title: Text(
-                "Joke",
-                textAlign: TextAlign.center,
-              ),
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+    return BlocProvider(
+      create: (context) => JokeBloc(
+        RepositoryProvider.of<JokeRepository>(context),
+      )..add(LoadJokeEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('The Joke App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const ExpansionTile(
+                title: Text(
+                  "Joke",
+                  textAlign: TextAlign.center,
                 ),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                //TODO Load New Joke
-              },
-              child: const Text('Load New Joke'),
-            ),
-          ],
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      "",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  //TODO Load New Joke
+                },
+                child: const Text('Load New Joke'),
+              ),
+            ],
+          ),
         ),
       ),
     );
