301d73acd6536ec2cbbd1e7bd5238ca71f544ed2
etczrn <etczrn@gmail.com>
2022-06-01 17:11:40 +0900

    Add states, events, and bloc for joke #35

--- bloc_with_joke/lib/bloc/joke_bloc/joke_bloc.dart
+++ bloc_with_joke/lib/bloc/joke_bloc/joke_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_with_joke/data/repositories/joke_repository.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+
+import '../../data/model/joke_model.dart';
 
 part 'joke_event.dart';
 part 'joke_state.dart';
 
 class JokeBloc extends Bloc<JokeEvent, JokeState> {
-  JokeBloc() : super(JokeInitial()) {
-    on<JokeEvent>((event, emit) {
-      // TODO: implement event handler
+  final JokeRepository _jokeRepository;
+
+  JokeBloc(this._jokeRepository) : super(JokeLoadingState()) {
+    on<LoadJokeEvent>((event, emit) async {
+      emit(JokeLoadingState());
+      try {
+        final joke = await _jokeRepository.getJoke();
+        emit(JokeLoadedState(joke));
+      } catch (e) {
+        emit(JokeErrorState(e.toString()));
+      }
     });
   }
 }
--- bloc_with_joke/lib/bloc/joke_bloc/joke_state.dart
+++ bloc_with_joke/lib/bloc/joke_bloc/joke_state.dart
@@ -1,6 +1,27 @@
 part of 'joke_bloc.dart';
 
 @immutable
-abstract class JokeState {}
+abstract class JokeState extends Equatable {}
 
-class JokeInitial extends JokeState {}
+class JokeLoadingState extends JokeState {
+  @override
+  List<Object?> get props => [];
+}
+
+class JokeLoadedState extends JokeState {
+  final JokeModel joke;
+
+  JokeLoadedState(this.joke);
+
+  @override
+  List<Object?> get props => [joke];
+}
+
+class JokeErrorState extends JokeState {
+  final String error;
+
+  JokeErrorState(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
