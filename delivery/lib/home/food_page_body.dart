d1c095aff03fdb9fb8a9c9ff2a9a9b76a879633b
etczrn <etczrn@gmail.com>
2022-06-02 22:28:47 +0900

    Set up default color for IconAndTextWidget #38

--- delivery/lib/home/food_page_body.dart
+++ delivery/lib/home/food_page_body.dart
@@ -1,3 +1,7 @@
+import 'package:delivery/uitls/colors.dart';
+import 'package:delivery/widgets/big_text.dart';
+import 'package:delivery/widgets/icon_and_text_widget.dart';
+import 'package:delivery/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -8,14 +12,15 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
-  PageController pageController = PageController(viewportFraction: 1.0);
+  PageController pageController = PageController(viewportFraction: 0.85);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      // color: Colors.red,
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -28,7 +33,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 220, // need stack to set height
-          margin: EdgeInsets.only(left: 5, right: 5),
+          margin: EdgeInsets.only(left: 10, right: 10, bottom: 30),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven ? Color(0xFf69c5df) : Color(0xff9294cc),
@@ -39,11 +44,57 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140, // need stack to set height
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            height: 120, // need stack to set height
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: 'Chinese Side'),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5,
+                            (index) => Icon(
+                                  Icons.star,
+                                  color: AppColors.mainColor,
+                                  size: 15,
+                                )),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: '4.5'),
+                      SizedBox(width: 10),
+                      SmallText(text: '1287'),
+                      SizedBox(width: 10),
+                      SmallText(text: 'Comments'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: 'Normal',
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: '1.7Km',
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: '32min',
+                          iconColor: AppColors.iconColor2)
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
