70f87a0603646a6ff204b2400a9bc5f93449b967
etczrn <etczrn@gmail.com>
2022-06-09 23:32:30 +0900

    Add two asynchronous methods and replace global variable with getProduct #47

--- app_architecture/lib/src/features/products/data/fake_products_repository.dart
+++ app_architecture/lib/src/features/products/data/fake_products_repository.dart
@@ -5,13 +5,33 @@ class FakeProductRepository {
   // * _() for singleton (private constructor)
   FakeProductRepository._();
   static FakeProductRepository instance = FakeProductRepository._();
+
+  final List<Product> _products = kTestProducts;
+
   List<Product> getProductList() {
-    return kTestProducts;
+    return _products;
   }
 
   // * Reason that the return type is nullabe is
   // * that we may or may not get a match depending on the ID
   Product? getProduct(String id) {
-    return kTestProducts.firstWhere((product) => product.id == id);
+    return _products.firstWhere((product) => product.id == id);
+  }
+
+  // * Methods returning a Future will begin with fetch
+  // * Suited for REST APIs
+  Future<List<Product>> fetchProductList() {
+    return Future.value(_products);
+  }
+
+  // * Methods returning a Stream will begin with watch
+  // * Suited for real time API based on web socket
+  Stream<List<Product>> watchProductList() {
+    return Stream.value(_products);
+  }
+
+  Stream<Product?> watchProduct(String id) {
+    return watchProductList()
+        .map((products) => products.firstWhere((product) => product.id == id));
   }
 }
