03f3418e62e7ce21cd28e49d782abab136d6200a
etczrn <etczrn@gmail.com>
2022-06-12 22:59:41 +0900

    Use LayoutBuilder to calculate radius #50

--- stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -3,18 +3,25 @@ import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class StopwatchRenderer extends StatelessWidget {
   final Duration elapsed;
-  const StopwatchRenderer({Key? key, required this.elapsed}) : super(key: key);
+  final double radius;
+  const StopwatchRenderer({
+    Key? key,
+    required this.elapsed,
+    required this.radius,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          color: Colors.indigo,
+          decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.orange),
+              borderRadius: BorderRadius.circular(radius)),
         ),
-        ElapsedTimeText(
-          elapsed: elapsed,
-        ),
+        // ElapsedTimeText(
+        //   elapsed: elapsed,
+        // ),
       ],
     );
   }
