db70c985a592a9b732f643ad955cf5cde9c350c6
etczrn <etczrn@gmail.com>
2022-06-12 23:25:31 +0900

    Create text marker #50

--- stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -23,6 +23,11 @@ class StopwatchRenderer extends StatelessWidget {
               left: radius,
               top: radius,
               child: ClockSecondsTickMarker(seconds: i, radius: radius)),
+        for (var i = 5; i <= 60; i += 5)
+          Positioned(
+              left: radius,
+              top: radius,
+              child: ClockTextMarker(value: i, maxValue: 60, radius: radius)),
         Positioned(
           left: radius,
           top: radius,
