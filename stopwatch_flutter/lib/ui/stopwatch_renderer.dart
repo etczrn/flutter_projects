36f797527e742de32aeae415bdf73334a3a758bc
etczrn <etczrn@gmail.com>
2022-06-12 23:08:41 +0900

    Create composable clockhand widget and use it #50

--- stopwatch_flutter/lib/ui/stopwatch_renderer.dart
+++ stopwatch_flutter/lib/ui/stopwatch_renderer.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:stopwatch_flutter/ui/clock_hand.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 
 class StopwatchRenderer extends StatelessWidget {
@@ -19,9 +22,23 @@ class StopwatchRenderer extends StatelessWidget {
               border: Border.all(width: 3, color: Colors.orange),
               borderRadius: BorderRadius.circular(radius)),
         ),
-        // ElapsedTimeText(
-        //   elapsed: elapsed,
-        // ),
+        Positioned(
+          left: radius,
+          top: radius,
+          child: ClockHand(
+              handLength: radius,
+              rotationZAngle: pi + (2 * pi / 60000) * elapsed.inMilliseconds,
+              handTickness: 2,
+              color: Colors.orange),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: radius * 1.3,
+          child: ElapsedTimeText(
+            elapsed: elapsed,
+          ),
+        ),
       ],
     );
   }
