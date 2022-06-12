0e0dc9d14064f127d9db698cd5403ee90a711580
etczrn <etczrn@gmail.com>
2022-06-12 17:06:25 +0900

    Create first provider #48

--- riverpod/lib/src/features/products/data/fake_products_repository.dart
+++ riverpod/lib/src/features/products/data/fake_products_repository.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeProductsRepository {
   FakeProductsRepository._();
@@ -28,3 +29,14 @@ class FakeProductsRepository {
         .map((products) => products.firstWhere((product) => product.id == id));
   }
 }
+
+// * Use provider to retrieve the repository
+// * How to create providers:
+// * - Delcare as a global variable
+// * - Specify a type annotation (Provider<T>)
+// * - Implement the body (anonymous function)
+// * The anonymous function can be quite complex
+// * and we can use the ref object to access other providers as dependencies
+final productRepositoryProvider = Provider<FakeProductsRepository>((ref) {
+  return FakeProductsRepository.instance;
+});
