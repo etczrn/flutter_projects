d764034f7b7cb2e62f060bbf51bfaed6a307bd14
etczrn <etczrn@gmail.com>
2022-06-12 22:42:32 +0900

    Use Ticker to replace original Timer #50

--- stopwatch_flutter/lib/ui/stopwatch.dart
+++ stopwatch_flutter/lib/ui/stopwatch.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text.dart';
 import 'package:stopwatch_flutter/ui/elapsed_time_text_basic.dart';
 
@@ -9,30 +10,35 @@ class Stopwatch extends StatefulWidget {
   _StopwatchState createState() => _StopwatchState();
 }
 
-class _StopwatchState extends State<Stopwatch> {
+class _StopwatchState extends State<Stopwatch>
+    with SingleTickerProviderStateMixin {
   // * The reason we declare this variable as late is
   // * that _initialTime is non-nullable variable
   // * but it is only initialized in initState,
   // * not when it's declared.
-  late DateTime _initialTime;
   Duration _elapsed = Duration.zero;
-  late final Timer _timer;
+  late final Ticker _ticker;
 
   @override
   void initState() {
     super.initState();
-    _initialTime = DateTime.now();
-    _timer = Timer.periodic(Duration(milliseconds: 50), (_) {
-      final now = DateTime.now();
+    // * Ticker automatically syncs to the screen refresh rate
+    // * and makes it easier to get the elapsed time
+
+    // * Ticker is a great solution when you want to update the UI every frame
+    // * The Flutter animation framework uses tickers under the hood
+    // * Do not use Ticker to redraw any part of the UI that doesn't change
+    _ticker = this.createTicker((elapsed) {
       setState(() {
-        _elapsed = now.difference(_initialTime);
+        _elapsed = elapsed;
       });
     });
+    _ticker.start();
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _ticker.dispose();
     super.dispose();
   }
 
