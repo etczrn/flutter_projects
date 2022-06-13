c5c064c2b458b163972382a74e9e3fb3a988d5a8
etczrn <etczrn@gmail.com>
2022-06-13 23:28:53 +0900

    Initialize project #53

---
+++ habit_tracker_flutter_with_riverpod/lib/persistence/hive_data_store.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:habit_tracker_flutter/models/task.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HiveDataStore {
+  static const tasksBoxName = 'tasks';
+
+  Future<void> init() async {
+    await Hive.initFlutter();
+    // register adapters
+    Hive.registerAdapter<Task>(TaskAdapter());
+    // open boxes
+    await Hive.openBox<Task>(tasksBoxName);
+  }
+
+  Future<void> createDemoTasks({
+    required List<Task> tasks,
+    bool force = false,
+  }) async {
+    final box = Hive.box<Task>(tasksBoxName);
+    if (box.isEmpty || force) {
+      await box.clear();
+      await box.addAll(tasks);
+    } else {
+      print('Box already has ${box.length} items');
+    }
+  }
+
+  ValueListenable<Box<Task>> tasksListenable() {
+    return Hive.box<Task>(tasksBoxName).listenable();
+  }
+}
---
+++ habit_tracker_flutter_with_riverpod/lib/ui/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker_flutter/models/task.dart';
+import 'package:habit_tracker_flutter/persistence/hive_data_store.dart';
+import 'package:habit_tracker_flutter/ui/home/tasks_grid_page.dart';
+import 'package:hive/hive.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final dataStore = HiveDataStore();
+    return ValueListenableBuilder(
+      valueListenable: dataStore.tasksListenable(),
+      builder: (_, Box<Task> box, __) => TasksGridPage(
+        tasks: box.values.toList(),
+      ),
+    );
+  }
+}
