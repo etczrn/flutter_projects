1e1e225548c72d44756b7643f9477ee726d0d3f7
etczrn <etczrn@gmail.com>
2022-06-16 22:11:35 +0900

    Add comments on StateNotifier #54

--- ecommerce_app_riverpod_part2/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app_riverpod_part2/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,6 +1,15 @@
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// * StateNotifier is a very important class for state management in Riverpod.
+// * It is an observable class that stores a single immutable state
+// * (unlike ChangeNotifier which is mutable and can store multiple state variables).
+// * Things to notice:
+// * - Always declare StateNotifier with a type (in this case, AsyncValue<void>)
+// * - Always pass an initial value in the constructor
+// * - The state should be set with the assigment operator (=).
+// * This will cause any listener widgets to rebuild
+// * - Always use StateNotifierProvider to declare the corresponding provider:
 class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   AccountScreenController({required this.authRepository})
       : super(const AsyncValue<void>.data(null));
