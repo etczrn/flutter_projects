4edc012898564ace6b459c05653f015a5c990fd7
etczrn <etczrn@gmail.com>
2022-06-22 23:26:37 +0900

    Show toolbar and title #56

--- riverpod_example/lib/home.dart
+++ riverpod_example/lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_example/item.dart';
+import 'package:riverpod_example/toolbar.dart';
 
 import 'providers/todo.dart';
 
@@ -22,7 +23,7 @@ class Home extends HookConsumerWidget {
         body: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
           children: [
-            // const Title(),
+            const Title(),
             TextField(
               key: addTodoKey,
               controller: newTodoController,
@@ -35,7 +36,7 @@ class Home extends HookConsumerWidget {
               },
             ),
             const SizedBox(height: 42),
-            // const Toolbar(),
+            const Toolbar(),
             if (todos.isNotEmpty) const Divider(height: 0),
             for (var i = 0; i < todos.length; i++) ...[
               if (i > 0) const Divider(height: 0),
@@ -54,6 +55,24 @@ class Home extends HookConsumerWidget {
             ],
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'todos',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Color.fromARGB(38, 47, 47, 247),
+        fontSize: 100,
+        fontWeight: FontWeight.w100,
+        fontFamily: 'Helvetica Neue',
       ),
     );
   }
