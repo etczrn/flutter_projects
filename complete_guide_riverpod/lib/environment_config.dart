b3c22f28cf4075618d374d1747ec9770a7957121
etczrn <etczrn@gmail.com>
2022-07-02 22:50:42 +0900

    Generate api key in EnvironmentConfig class #65

---
+++ complete_guide_riverpod/lib/environment_config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EnvironmentConfig {
+  // * We add the api key
+  // * by running 'flutter run --dart-define=movieApiKey=<your_api_key>'
+  final movieApiKey = const String.fromEnvironment("movieApiKey");
+}
+
+final environmentConfigProvider = Provider<EnvironmentConfig>((ref) {
+  return EnvironmentConfig();
+});
